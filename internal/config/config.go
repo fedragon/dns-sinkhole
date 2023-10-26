a02157e58bc8b9f8d526f6559e140fc348aae514
Federico Ragona <federico.ragona@gmail.com>
2023-10-26 21:26:54 +0200

    chore: disable debug endpoint by default

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,7 +4,7 @@ type Config struct {
 	Addr                 string `envconfig:"SINKHOLE_ADDR" default:"0.0.0.0:1153"`
 	FallbackAddr         string `envconfig:"FALLBACK_ADDR" default:"1.1.1.1:53"`
 	HostsPath            string `envconfig:"HOSTS_PATH" default:"./hosts"`
-	DebugEndpointEnabled bool   `envconfig:"DEBUG_ENDPOINT_ENABLED" default:"true"`
+	DebugEndpointEnabled bool   `envconfig:"DEBUG_ENDPOINT_ENABLED" default:"false"`
 	MetricsEnabled       bool   `envconfig:"METRICS_ENABLED" default:"true"`
 	HttpAddr             string `envconfig:"HTTP_ADDR" default:"0.0.0.0:8000"`
 }
