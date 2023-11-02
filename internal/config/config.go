c0d93526d9f41f515f22bbcd97f31a32319ceeff
Federico Ragona <federico.ragona@gmail.com>
2023-11-02 22:18:38 +0100

    chore: disable HTTP server by default

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,5 +8,5 @@ type Config struct {
 	// HTTP server config: it will only be started if either DebugEndpointEnabled or MetricsEnabled is true
 	HttpServerAddr       string `envconfig:"HTTP_SERVER_ADDR" default:"0.0.0.0:8000"`
 	DebugEndpointEnabled bool   `envconfig:"DEBUG_ENDPOINT_ENABLED" default:"false"`
-	MetricsEnabled       bool   `envconfig:"METRICS_ENABLED" default:"true"`
+	MetricsEnabled       bool   `envconfig:"METRICS_ENABLED" default:"false"`
 }
