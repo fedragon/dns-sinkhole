3d409433c7099f1c998a6a66284056c08380bbcc
Federico Ragona <federico.ragona@gmail.com>
2023-11-03 21:40:13 +0100

    fix(install.sh): make comparison work

--- deploy/install.sh
+++ deploy/install.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env sh
 
 # clean up old sinkhole service (if any)
-active=$(systemctl is-active sinkhole.service 2>&1 /dev/null)
+active=$(systemctl is-active sinkhole.service 2> /dev/null)
 if [ "$active" = "active" ]; then
   echo "stopping existing sinkhole.service..."
   systemctl stop sinkhole.service
 fi
 
-enabled=$(systemctl is-enabled sinkhole.service 2>&1 /dev/null)
+enabled=$(systemctl is-enabled sinkhole.service 2> /dev/null)
 if [ "$enabled" = "enabled" ]; then
   echo "disabling existing sinkhole.service..."
   systemctl disable sinkhole.service
   systemctl daemon-reload
-  systemctl reset-failed sinkhole.service
 fi
 
 rm /etc/systemd/system/sinkhole.service
